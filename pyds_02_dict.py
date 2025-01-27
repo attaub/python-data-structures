e493116b6e529b5ed4f027e0fd4ea20b84e134b8
attamhb <attahassani@hotmail.com>
2025-01-27 00:46:00 -0700

    created a dictionary and dataframe  from iris data and

--- pyds_02_dict.py
+++ pyds_02_dict.py
@@ -1,5 +1,5 @@
 my_dict = {'name': 'Alice', 'age': 25, 'city': 'New York'}
-my_dict_copy = my_dict.copy()  
+my_dict_copy = my_dict.copy()
 my_dict_copy.clear()
 my_dict_copy = my_dict.copy()
 
@@ -23,10 +23,25 @@ my_dict.update(new_data)
 my_dict
 my_dict.values()
 
-del my_dict['age']  
-my_dict.clear()  
-new_dict = dict.fromkeys(['x', 'y', 'z'], 0)  
-my_dict = dict(a=1, b=2, c=3)  
+del my_dict['age']
+my_dict.clear()
+new_dict = dict.fromkeys(['x', 'y', 'z'], 0)
+my_dict = dict(a=1, b=2, c=3)
 
 for key, value in my_dict.items():
     print(f'{key}: {value}')
+
+
+from sklearn import datasets
+import pandas as pd
+
+df = pd.DataFrame()
+iris = datasets.load_iris()
+X = iris.data
+keys = ['sepla_length', 'sepal_width', 'peta_length', 'petal_width']
+my_dict = {}
+for i in range(4):
+    my_dict[f'{keys[i]}'] = X[:, i]
+
+my_dict['label'] = iris.target
+df = pd.DataFrame(my_dict)
