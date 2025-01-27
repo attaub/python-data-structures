065e67d948716c921c93f18eee2d8962f3e30ad7
attamhb <attahassani@hotmail.com>
2025-01-27 01:44:16 -0700

    refactored sliding window code

--- pyds_07_two_pointers.py
+++ pyds_07_two_pointers.py
@@ -1,35 +1,24 @@
-# Process data from both ends towards center (or from center towards ends)
-# An efficient way to reduce time complexity
-# Finding if a list has two numbers that sum up to a given target
+# Process data from both ends towards center or vice versa
+
 
 def two_sum(nums, target):
     nums.sort()
-    left = 0
-    right = len(nums) - 1
+    l, r = 0, len(nums) - 1
 
-    # Loop until the two pointers meet
-    while left < right:
-        current_sum = nums[left] + nums[right]
+    while l < r:
+        cur_s = nums[l] + nums[r]
 
-        # Check if found the pair
-        if current_sum == target:
-            return [nums[left], nums[right]]  # Return the pair
+        if cur_s == target:
+            return [nums[l], nums[r]]
 
-        # If sum small, move left pointer to right
-        elif current_sum < target:
-            left += 1
+        elif cur_s < target:
+            l += 1
 
-        # If sum is large, move right pointer to left
         else:
-            right -= 1
-
+            r -= 1
     return None
 
 
-
-
-arr = [1, 5, 2, 3, 2, 7, 4]
-target = 6
-
-print(two_sum(arr, target))
-
+nums = [1, 5, 2, 3, 2, 7, 4]
+two_sum(nums, 6)
+two_sum(nums, 11)
