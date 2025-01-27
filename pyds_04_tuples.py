d8e16a01dae7b812b98cf71ba99cf2f778d8c0fd
attamhb <attahassani@hotmail.com>
2025-01-27 01:02:23 -0700

    refactored pyds_04_tuples.py

--- pyds_04_tuples.py
+++ pyds_04_tuples.py
@@ -1,43 +1,14 @@
-mytuple = ("apple", "banana", "cherry")
-("apple", "banana", "cherry") + ("orange",)
+tup1 = ("apple", "banana", "cherry")
+tup2 = ("apple", "banana", "cherry") + ("orange",)
+
+tup = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = tup
+(green, *tropic, red) = tup
+
+single_tup = (42,)
+empty_tup = ()
 
 
-fruits = ("apple", "banana", "cherry")
-(green, yellow, red) = fruits
-
-fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
-(green, yellow, *red) = fruits
-
-fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
-
-
-(green, *tropic, red) = fruits
-
-
-
-tuple1 = (1, 2, 3, 4)
-tuple2 = ("apple", 3.14, 10)
-single_element_tuple = (42,)
-empty_tuple = ()
-
-
-# Using count() method
-tuple3 = (1, 2, 2, 3, 4, 2)
-count_of_2 = tuple3.count(2)
-
-
-# Using index() method
-index_of_3 = tuple3.index(3)
-
-try:
-    tuple3.index(5)
-except ValueError as e:
-    print("Error:", e)
-
-# Iterating through tuple
-print("Iterating through tuple3:")
-for item in tuple3:
-    print(item)
-
-
-
+tup = (1, 2, 2, 3, 4, 2)
+count_of_2 = tup.count(2)
+index_of_3 = tup.index(3)
